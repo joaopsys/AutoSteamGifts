b79a20bba877a7eb532a735787d30e53bfbad1f2
joaopsys <js@joao.sh>
2016-02-21 06:34:43 +0000

    - Save valid cookies (hidden file .steamgifts), thus the following script invocation can be performed without parameters

--- steamgifts.py
+++ steamgifts.py
@@ -7,6 +7,8 @@ import urllib.request
 import urllib.parse
 import sys
 import re
+import os
+import ctypes
 
 MAIN_URL = 'http://www.steamgifts.com'
 PAGING_URL = 'http://www.steamgifts.com/giveaways/search?page='
@@ -16,6 +18,7 @@ MAIN_REGEX = 'href="\/giveaway\/(?P<Code>[a-zA-Z0-9]*?)\/[^\/]*?"'
 XSRF_REGEX = 'name="xsrf_token" value="(?P<XSRF>[0-9a-zA-Z]*?)"'
 POINTS_REGEX = "<span class=\"nav__points\">(?P<Points>[0-9]+)</span>"
 POINTS_RESPONSE_REGEX = "\"points\":\"(?P<Points>[0-9]+)\""
+COOKIE_FILE_NAME = ".steamgifts"
 
 def getWebPage(url, headers, cookies, postData=None):
     try:
@@ -49,12 +52,34 @@ def nodup(seq):
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
 
+def saveCookie(cookie):
+    with open(COOKIE_FILE_NAME, 'w') as f:
+        f.write(cookie)
+
+## https://stackoverflow.com/questions/25432139/python-cross-platform-hidden-file
+    if os.name == 'nt':
+        ret = ctypes.windll.kernel32.SetFileAttributesW(COOKIE_FILE_NAME, "0x02")
+        if not ret:
+            raise ctypes.WinError()
+
+def getCookieFromfile():
+    try:
+        with open(COOKIE_FILE_NAME, 'r') as f:
+            return f.read()
+    except:
+        return ''
 def main():
     try:
         if (len(sys.argv) < 2):
-            print('./steamgifts.py <Cookie> [<SteamGifts Page Number (default=1)>]')
-            print('Please insert your cookie. Press CTRL+SHIFT+J on the website (CTRL+SHIFT+K on Firefox) and type document.cookie. Then paste the whole string as an argument.')
-            return
+            sgCookie = getCookieFromfile()
+            if (not sgCookie or len(sgCookie) < 1):
+                print('<<<AutoSteamGifts>>>')
+                print('Usage: ./steamgifts.py <Cookie> [<starting page (default=1)>]')
+                print('Please insert your cookie. Press CTRL+SHIFT+J on the website (CTRL+SHIFT+K on Firefox) and type document.cookie. Then paste the whole string as an argument.')
+                print('You only need need to do this once because AutoSteamGifts will remember your cookie (if valid).')
+                return
+        else:
+            sgCookie = sys.argv[1]
         regex = re.compile(MAIN_REGEX)
         xsrfRegex = re.compile(XSRF_REGEX)
         pointsRegex = re.compile(POINTS_REGEX)
@@ -66,10 +91,11 @@ def main():
             startingPage = 1
 
         while True:
-            homePage = getWebPage(PAGING_URL+str(startingPage), GLOBAL_HEADERS, sys.argv[1])
-            if (homePage is None):
-                print('An error occurred while fetching results (probably expired cookie?). Terminating...')
+            homePage = getWebPage(PAGING_URL+str(startingPage), GLOBAL_HEADERS, sgCookie)
+            if (not homePage):
+                print('An error occurred while fetching results (probably expired/invalid cookie). Terminating...')
                 return
+            saveCookie(sgCookie)
             xsrfToken = xsrfRegex.findall(str(homePage))[0]
             points = pointsRegex.findall(str(homePage))[0]
             gamesList = regex.findall(str(homePage))
@@ -80,7 +106,7 @@ def main():
                     print('You have 0 points, exiting...')
                     return
                 postData = {'xsrf_token':xsrfToken, 'do':'entry_insert', 'code':g}
-                postResponse = getWebPage(MAIN_URL+'/ajax.php',GLOBAL_HEADERS,sys.argv[1],postData)
+                postResponse = getWebPage(MAIN_URL+'/ajax.php',GLOBAL_HEADERS,sgCookie,postData)
                 points = pointsResponseRegex.findall(str(postResponse))[0]
                 print(postResponse)
             startingPage = int(startingPage)+1
